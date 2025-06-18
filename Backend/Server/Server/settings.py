38e98ab07f261170e6d00ef9554137ecba7c72a5
Ritwik Chawla <73139315+ritwikchawla@users.noreply.github.com>
2025-06-18 11:39:27 +0530

    Update settings.py

--- Backend/Server/Server/settings.py
+++ Backend/Server/Server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_e4m%j2i0hrz@mqf7#^o3ofw807$d*y0rc0rf%h$xhz*+t^j=l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['vercel.app']
 
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
