407a64c2dbacbe0dfc4e9eb5e37b47a79912f5c0
Ritwik Chawla <73139315+ritwikchawla@users.noreply.github.com>
2025-06-20 11:14:12 +0530

    Update wsgi.py

--- Backend/Server/Server/wsgi.py
+++ Backend/Server/Server/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+# add this vercel variable
+app = application
