1be469d51ad7282d7935b86cfb9de90d5259c3ee
Ritwik Chawla <73139315+ritwikchawla@users.noreply.github.com>
2025-06-20 11:14:40 +0530

    Update settings.py

--- Backend/Server/Server/settings.py
+++ Backend/Server/Server/settings.py
@@ -72,18 +72,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Server.wsgi.app'
+WSGI_APPLICATION = 'Server.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
