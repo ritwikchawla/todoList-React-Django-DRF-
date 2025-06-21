8fc1f4cd0fb93e397a3f47e8c03e4a359d23a748
Ritwik Chawla <chawla.ritwik@gamil.com>
2025-06-21 11:08:47 +0530

    21 june (2)

--- Backend/Server/Server/wsgi.py
+++ Backend/Server/Server/wsgi.py
@@ -14,9 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Server.settings')
 
 application = get_wsgi_application()
-# add this vercel variable
-<<<<<<< HEAD
-app = application
-=======
-app = application
->>>>>>> 92b89afad7f6f4bdb118f6d35dc51dae20a04d09
+
